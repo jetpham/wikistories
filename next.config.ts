9c8e7863df10de089af6bf4642456b1ad203fef9
Jet Pham <55770902+jetpham@users.noreply.github.com>
2025-05-05 18:50:49 -0700

    redirect /stories to /

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
 
 module.exports = {
     reactStrictMode: false,
+    async redirects() {
+        return [
+          {
+            source: '/stories',
+            destination: '/',
+            permanent: true,
+          },
+        ]
+      },
 }
 
 export default nextConfig;
